64b295318c43874f4b9f4e67f4a88581b6d2101a
Raghubabu Jamana <113964677+Raghubabu7993@users.noreply.github.com>
2023-12-21 12:37:27 +0530

    Update arraysorting.sv

--- arraysorting.sv
+++ arraysorting.sv
@@ -4,10 +4,23 @@ class sr;
 	int a;
 	constraint c{q.size inside {[10:20]};}
 	constraint c1{foreach(q[i])
-	                q[i] inside {[1:100]};}
+		        q[i] inside {[1:100]};}
 	function void post_randomize();
 	    $display("elements in array %0p",q);
 		q.sort();
+		//with out using sort method
+		foreach(a[i])
+			begin
+				for(int j=i+1;j<$size(a);j++)
+					begin
+						if(a[i]>a[j])
+							begin
+								a[i]=a[i]+a[j];
+								a[j]=a[i]-a[j];
+								a[i]=a[i]-a[j];
+							end
+					end
+			end
 		a=q[$-1];
 		$display("second largest num is %0d",a);
 	endfunction
